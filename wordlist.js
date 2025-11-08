c3be02ba9c73de1ce4a7819aef86bf6559145de1
Aarush Prakash <aarushprakash3@gmail.com>
2025-11-07 20:11:00 -0500

    Create wordlist.js

---
+++ wordlist.js
@@ -0,0 +1,6 @@
+const WORDS = [
+  "apple","chair","river","lucky","ocean","paper","night","green",
+  "quiet","star","mount","table","coffee","window","silver","garden",
+  "clock","lemon","piano","cloud","river","forest","sun","moon",
+  "tree","flower","stone","sky","rain","wind","fire"
+];
